b44fce0afa47d962dae35eb6dcfbba67721c5456
Enzo Gabriel <enzoddos7@gmail.com>
2023-10-17 20:46:29 -0300

    Requisições GET

--- src/main/java/curso/spring/api/controller/MedicoController.java
+++ src/main/java/curso/spring/api/controller/MedicoController.java
@@ -5,11 +5,13 @@ import curso.spring.api.medico.*;
 import curso.spring.api.medico.MedicoRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("medicos")
@@ -23,4 +25,8 @@ public class MedicoController {
         repository.save(new Medico(dados));
     }
 
+    @GetMapping
+    public Page<DadosListagemMedico> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
+        return repository.findAll(paginacao).map(DadosListagemMedico::new);
+    }
 }
